bdf9011db0de6c55f8fd39f8d08f18f5cda39333
sadam <sadamccr@gmail.com>
2022-12-20 20:20:04 +0500

    login, register, main css files added..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom'
+import Login from './components/login';
+import Registration from './components/registration';
+import './main.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to={'/login'}>Login</Link>
+        <Link to={'/registration'}>Register</Link>
+      </nav>
+      <Routes>
+        <Route path='/login' element={<Login></Login>} />
+        <Route path='/registration' element={<Registration></Registration>} />
+      </Routes>
+    </Router>
   );
 }
 
